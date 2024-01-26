edcd1a2cd301dc8811529803168366b9b0b213ef
anup rana <anupranamagar20@gmail.com>
2024-01-27 02:19:41 +0545

    add tailwind css

--- image-main/src/pages/Login.jsx
+++ image-main/src/pages/Login.jsx
@@ -6,6 +6,7 @@ const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [redirects, setRedirects] = useState(false);
+  const [showPass, setShowPass] = useState(false);
 
   const submit = async (e) => {
     e.preventDefault();
@@ -33,28 +34,54 @@ const Login = (props) => {
   return (
     <>
       <Nav />
-      <form onSubmit={submit}>
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Email address"
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
+      <div className="-z-10 shadow-md border-2 w-[25rem] felx  flex-col p-5 justify-center h-[25rem] px-10 absolute top-[50%] left-[50%]  translate-x-[-50%] translate-y-[-50%] ">
+        <form
+          onSubmit={submit}
+          className="flex flex-col justify-center items-center gap-y-4 "
+        >
+          <h2 className="h3 mb-3 ">Please Log in</h2>
+          <input
+            type="email"
+            className="border-2 border-black rounded-md px-2 py-1 w-72"
+            placeholder="Email address"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Password"
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
-          Sign in
-        </button>
-      </form>
+          <input
+            type={showPass ? "text" : "password"}
+            className="border-2 border-black rounded-md px-2 py-1 w-72"
+            placeholder="Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label>
+            <input
+              type="checkbox"
+              value={showPass}
+              onChange={() => {
+                setShowPass(!showPass);
+              }}
+            />
+            Show password
+          </label>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded px-4 py-2"
+            type="submit"
+          >
+            Log in
+          </button>
+        </form>
+        <p className="text-center mt-8">
+          Not signd up yet?{" "}
+          <a
+            href="/signup"
+            className="font-bold hover:underline cursor-pointer"
+          >
+            Sign up
+          </a>
+        </p>
+      </div>
     </>
   );
 };
